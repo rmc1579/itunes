c6c9a33fee6aeefde3f7d5152032d8bf28d4a4c9
rmc1579 <rmc1579@gmail.com>
2015-10-22 20:10:20 -0600

    Adding itunes project with answers

--- js/itunesService.js
+++ js/itunesService.js
@@ -10,5 +10,42 @@ app.service('itunesService', function($http, $q){
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+    this.searchArtist = function (artist){
+        var defer = $q.defer();
+        var songsArray = [];
+        console.log('good');
+        var url = "https://itunes.apple.com/search?term=" + artist + "&callback=JSON_CALLBACK";
+        console.log(url);
+        $http.jsonp("http://itunes.apple.com/search",{
+            params:{
+                "callback": "JSON_CALLBACK",
+                "term": artist
+            }
+        })
+        .then(function(response){
+            
+            var oneEntry = function(artistName, collectionName, artwork, kind, collectionPrice, previewUrl, trackName){
+                this.Artist = artistName;
+                this.Collection = collectionName;
+                this.Album = artwork;
+                this.Type = kind;
+                this.Collection = collectionPrice;
+                this.Play = previewUrl;
+                this.Song = trackName;
+            };
+            
+            var getR = response.data.results;
+            
+            for (var i = 0; i<getR.length; i++){
+                var newSong = new oneEntry(getR[i].artistName, getR[i].collectionName, getR[i].artworkUrl100, getR[i].kind, getR[i].collectionPrice, getR[i].previewUrl, getR[i].trackName);
+                songsArray.push(newSong);
+                newSong = null;
+            }
+            defer.resolve(songsArray); //this gets return after response data gets parse, returns an array of objects
+            
+            
+        });
+        return defer.promise; //this gets return right a way. 
+       
+    };
 });
